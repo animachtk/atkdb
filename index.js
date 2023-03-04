27dba1dccdd6773eb13f7f2db1797969aad6a2a4
animachtk <51044695+animachtk@users.noreply.github.com>
2023-03-04 14:16:29 +0400

    fix for uploaded db

--- index.js
+++ index.js
@@ -1,87 +1,10 @@
-const express = require('express')
-const app = express()
-const db = require('@cyclic.sh/dynamodb')
-const APP_TOKEN = process.env.API_AUTH_TOKEN;
-
+const express = require('express');
+const app = express();
+const db = require('./db.js');
+const CDB = db.CDB;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-async function updateMany(table,key,data,res){
-try{
-item = await table.get(key);
-if(!item){
-	item = await table.set(key, {data:[]})
-}
-item=item.props.data;
-if(Array.isArray(data) && data.length>0){
-	for(let a of data){
-		item.push(a);
-	}
-}
-let deleteCount=0;
-if(item.length>3e3){
-	let count = 1;
-	let re = /(\-([0-9]))$/;
-	let newcount=key.match(re);
-	
-	if(newcount){
-	count=Number(newcount[2])+1;
-	key=newcount.input.replace(re,'');
-	}
-	
-	key=key+"-"+count;
-	updateMany(table,key,data);
-	return
-/*deleteCount=item.length-3e3;
-item.splice(0,deleteCount);*/
-}
-		
-item = await table.set(key, {data:item})
-res.json({msg: "Успешно загружено "+data.length+" новых объекта(ов). Всего - "+item.props.data.length+" объекта(ов). Удалено из начала: "+deleteCount+" объекта(ов)."}).end()	
-}catch(e){
-console.log(e)
-res.json({msg: "Ошибка при сохранении "}).end()	
-}
-}	
-
-async function CDB(res,m,col,key,data){
-	let table = db.collection(col);
-	let item = false;
-	switch(m){
-	case "update":
-		item = await table.get(key);
-		
-		if(!item){
-		  item = await table.set(key, {data:[]})
-		}
-		
-		item=item.props.data;
-		item.push(data);
-		item = await table.set(key, {data:item})
-	res.json(item).end()	
-	break;
-	
-	case "updateMany":
-	updateMany(table,key,data,res)	
-	break;
-	
-	case "delete":
-	item = await table.delete(key);
-	res.json(item).end()
-	break;
-	
-	case "list":
-	
-	item = await table.list();
-	res.json(item).end()
-	break;
-	
-	default:
-	item = await table.get(key);
-  res.json(item).end()
-	break;
-	}
-}
 
 app.post('/:hash/:method/:col/:key', async (req, res) => {
   const {method,hash,col,key} = req.params;
