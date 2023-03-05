4cd4dd64e945ff944627bb7e7a29f7598dc39e92
animachtk <51044695+animachtk@users.noreply.github.com>
2023-03-05 18:10:37 +0400

    added new method updateKeys

--- db.js
+++ db.js
@@ -45,6 +45,44 @@ return {msg: "Ошибка при сохранении."};
 }
 }	
 
+async function updateKeys(table,ch,data){
+try{
+
+let keys = Object.keys(data);
+
+for(let key of keys){
+
+item = await table.get(key);
+
+if(!item){
+	item = await table.set(key, {data:[]})
+}
+
+item=item.props.data;
+
+if(Array.isArray(data[key]) && data[key].length>0){
+	for(let a of data[key]){
+		item.push(a);
+	}
+}
+
+let deleteCount=0;
+if(item.length>3e3){
+deleteCount=item.length-3e3;
+item.splice(0,deleteCount);
+}	
+item = await table.set(key, {data:item});
+
+console.log("Успешно загружено в "+key+" "+data[key].length+" новых объекта(ов). Всего - "+item.props.data.length+" объекта(ов). Удалено из начала: "+deleteCount+" объекта(ов).");
+	
+}
+return {msg: "Успешно обновлено "+keys.length+" новых объекта(ов)."};
+}catch(e){
+console.log(e)
+return {msg: "Ошибка при сохранении."};
+}
+}
+
 async function CDB(res,m,col,key,data){
 	
 	let table = db.collection(col);
@@ -67,6 +105,11 @@ async function CDB(res,m,col,key,data){
 	
 	case "updateMany":
 		item = await updateMany(table,key,data);
+		res.json(item).end()
+	break;
+			
+	case "updateKeys":
+		item = await updateKeys(table,key,data);
 		res.json(item).end()
 	break;
 	
@@ -147,83 +190,7 @@ async function CDB(res,m,col,key,data){
 		res.json(item).end()
 	}
 	break;
-			
-	case "restructure":
-	if(key){
-		item = await table.list();
-		results=item.results;
-		let keys = [];
-		for(let k of results){
-		
-			if(k.key.indexOf(key)>-1){
-			keys.push(k.key)
-			}
-			
-			if(keys.length>0){
-			
-				let items=[];
-				
-				for(let a of keys){
-					item = await table.get(a);
-					let temparr=item?.props?.data||[];
-					if(temparr.length>0){
-						for(let i of temparr){
-							items.push(i)
-						}
-					}
 
-				}
-				console.log(items.length);
-				if(items.length>0){
-				
-					let USERS={};
-
-
-					for(let i of items){
-					let name=i.username;
-					let item=i;
-					delete item.username;
-					if(!USERS.hasOwnProperty(name)){
-					USERS[name]=[]
-					}
-					USERS[name].push(item);
-					}
-
-					let arr=Object.keys(USERS);
-					
-					for(let user of arr){
-						let collection = db.collection("random-usermessages");
-						
-						item = await collection.get(user);
-						
-						if(!item){
-							item = await collection.set(user, {data:[]})
-						}
-						
-						item=item.props.data;
-						
-						for(let a of USERS[user]){
-						//let test=item.find(e => e.time===a.time);
-						//if(!test){
-						item.push(a);
-						//}
-						}
-						
-						let colitem = await collection.set(key, {data:USERS[user]});
-						
-						
-						
-						console.log(user+" updated "+USERS[user].length)
-
-					}
-					
-					
-				}
-			}
-		}
-	}
-	break;
-	
 	default:
 		item = await table.get(key);
 		res.json(item).end()
